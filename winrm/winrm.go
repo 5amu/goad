d8391e9d1da1e3adcf523440b35d560715ff9207
5amu <casalinovalerio.cv@gmail.com>
2024-01-24 00:06:14 +0100

    misc

--- winrm/winrm.go
+++ winrm/winrm.go
@@ -1,1 +1,29 @@
 package winrm
+
+type Transport string
+
+const (
+	PLAINTEXT Transport = "plaintext"
+	KERBEROS  Transport = "kerberos"
+	SSL       Transport = "ssl"
+	NTLM      Transport = "ntlm"
+	CREDSSP   Transport = "credssp"
+)
+
+type WinrmClient struct {
+	Host       string
+	Trans      Transport
+	Username   string
+	Password   string
+	Realm      string
+	Service    string
+	Keytab     string
+	Timeout    int
+	KDC        string
+	Encryption bool
+	Proxy      string
+}
+
+func (c *WinrmClient) OpenShell() error {
+	return nil
+}
