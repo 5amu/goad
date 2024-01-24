6e5d26647bec2f01f4c486f015f73c36b45903d4
5amu <casalinovalerio.cv@gmail.com>
2024-01-24 01:07:56 +0100

    polish library

--- ldap/ldap.go
+++ ldap/ldap.go
@@ -19,44 +19,15 @@ type LdapClient struct {
 	SkipTLS    bool
 }
 
-func (lc *LdapClient) connectTLS() error {
-	config := &tls.Config{
-		InsecureSkipVerify: true,
-		ServerName:         lc.ServerName,
+func NewLdapClient(host string, port int, realm string, ssl bool, skiptls bool) *LdapClient {
+	return &LdapClient{
+		Host:    host,
+		Port:    port,
+		Realm:   realm,
+		BaseDN:  fmt.Sprintf("dc=%s", strings.Join(strings.Split(realm, "."), ",dc=")),
+		SkipTLS: skiptls,
+		UseSSL:  ssl,
 	}
-	l, err := ldap.DialTLS("tcp", fmt.Sprintf("%s:%d", lc.Host, lc.Port), config)
-	if err != nil {
-		return err
-	}
-	lc.Conn = l
-	return nil
-}
-
-func (lc *LdapClient) connect() error {
-	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", lc.Host, lc.Port))
-	if err != nil {
-		return err
-	}
-
-	// Reconnect with TLS
-	if !lc.SkipTLS {
-		err = l.StartTLS(&tls.Config{InsecureSkipVerify: true})
-		if err != nil {
-			return err
-		}
-	}
-	lc.Conn = l
-	return nil
-}
-
-func (lc *LdapClient) Connect() error {
-	if lc.Conn != nil {
-		return nil
-	}
-	if lc.UseSSL {
-		return lc.connectTLS()
-	}
-	return lc.connect()
 }
 
 // Close closes the ldap backend connection.
@@ -68,52 +39,58 @@ func (lc *LdapClient) Close() {
 	lc.Conn = nil
 }
 
-func (lc *LdapClient) bind(username, password string, prefix string) error {
-	splitted := strings.Split(prefix, ".")
-	tentativeUser := fmt.Sprintf("%s\\%s", prefix, username)
-	switch password {
-	case "":
-		if len(splitted) == 1 {
-			if err := lc.Conn.UnauthenticatedBind(tentativeUser); err != nil {
-				return lc.Conn.UnauthenticatedBind(username)
-			}
-			return nil
-		}
-		if err := lc.Conn.UnauthenticatedBind(tentativeUser); err != nil {
-			return lc.bind(username, password, strings.Join(splitted[:len(splitted)-1], "."))
-		}
-	default:
-		if len(splitted) == 1 {
-			return lc.Conn.Bind(tentativeUser, password)
-		}
-		if err := lc.Conn.Bind(tentativeUser, password); err != nil {
-			return lc.bind(username, password, strings.Join(splitted[:len(splitted)-1], "."))
-		}
+func (c *LdapClient) Connect() error {
+	if c.Conn != nil {
+		return nil
+	}
+
+	var err error
+	if c.UseSSL {
+		c.Conn, err = ldap.DialTLS("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port), &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         c.Host,
+		})
+	} else {
+		c.Conn, err = ldap.Dial("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port))
+	}
+	if err != nil {
+		return err
+	}
+
+	if c.UseSSL {
+		l, err := ldap.DialTLS("tcp", fmt.Sprintf("%s:%d", c.Host, c.Port), &tls.Config{
+			InsecureSkipVerify: true,
+			ServerName:         c.Host,
+		})
+		c.Conn = l
+		return err
 	}
 	return nil
 }
 
 // Authenticate authenticates the user against the ldap backend.
-func (lc *LdapClient) Authenticate(username, password string) error {
-	if lc.Conn == nil {
-		if err := lc.Connect(); err != nil {
+func (c *LdapClient) Authenticate(username, password string) error {
+	if c.Conn == nil {
+		if err := c.Connect(); err != nil {
 			return err
 		}
 	}
 
-	if err := lc.Conn.NTLMBind(lc.Realm, username, password); err == nil {
+	if err := c.Conn.NTLMBind(c.Realm, username, password); err == nil {
 		return nil
 	}
 
-	if err := lc.bind(username, password, lc.Realm); err != nil {
-		return err
+	switch password {
+	case "":
+		if err := c.Conn.UnauthenticatedBind(username); err != nil {
+			return err
+		}
+	default:
+		if err := c.Conn.Bind(username, password); err != nil {
+			return err
+		}
 	}
-
-	user, err := lc.FindObject(username)
-	if err != nil {
-		return err
-	}
-	return lc.Conn.Bind(user["dn"], password)
+	return nil
 }
 
 func (lc *LdapClient) AuthenticateNTLM(username, hash string) error {
@@ -130,15 +107,4 @@ func (lc *LdapClient) Search(filter string, attributes ...string) (*ldap.SearchR
 		lc.BaseDN, ldap.ScopeWholeSubtree, ldap.NeverDerefAliases,
 		0, 0, false, filter, attributes, nil,
 	))
-}
-
-func NewLdapClient(host string, port int, realm string, ssl bool, skiptls bool) *LdapClient {
-	return &LdapClient{
-		Host:    host,
-		Port:    port,
-		Realm:   realm,
-		BaseDN:  fmt.Sprintf("dc=%s", strings.Join(strings.Split(realm, "."), ",dc=")),
-		SkipTLS: skiptls,
-		UseSSL:  ssl,
-	}
 }
